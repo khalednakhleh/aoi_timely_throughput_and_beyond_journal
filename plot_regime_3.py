42d7871c2112ee4c6489d7638456a8a4ec24da1c
khaled nakhleh <khaled.jkn@gmail.com>
2023-03-27 00:25:18 -0500

    added regime 3 results

--- plot_regime_3.py
+++ plot_regime_3.py
@@ -6,17 +6,17 @@ import matplotlib as mpl
 #mpl.use('pgf')
 # for plotting regime 3.
 
-num_clients = [5]#, 10, 20]
+num_clients = [5, 10, 20]
 
 timeslots = 1000000000
-policies = [6]#,1,4]
+policies = [6,1,4]
 
 
 labels = ['VWD', 'WLD', 'DBLDF']
 
 theoretical_labels = ['Theoretical VWD', 'Theoretical WLD', 'Theoretical DBLDF']
 
-num_runs = 1
+num_runs = 5
 plotting_interval = 2000000
 
 graph_interval = 100000000 # for the x ticks 
@@ -98,13 +98,15 @@ def average_results(current_policy, current_num_clients):
         weights = weights_twenty_clients
 
     directory = (f"results/num_clients_{current_num_clients}_regime_{regime_selection}/")
+    #print(directory)
     
     clients_value_per_policy = []
     for current_client in np.arange(1, current_num_clients+1):
         client_avg_over_runs = []
-        for current_run in np.arange(5, num_runs + 1):
+        for current_run in np.arange(1, num_runs + 1):
 
             df = pd.read_csv(directory+f"client_{current_client}_run_{current_run}_policy_{current_policy}_regime_{regime_selection}_results.txt", header=None)
+           
             value_to_plot = df.iloc[:,0]
 
             client_avg_over_runs.append(value_to_plot)
@@ -114,10 +116,13 @@ def average_results(current_policy, current_num_clients):
 
         client_avg_over_runs = np.divide(client_avg_over_runs, num_runs)
 
+        client_avg_over_runs = client_avg_over_runs / np.arange(1, timeslots+plotting_interval, plotting_interval)
 
-        client_avg_over_runs = client_avg_over_runs + (weights[current_client-1]* delays[current_client-1]**2)
+
+        client_avg_over_runs = client_avg_over_runs + (weights[current_client-1] * delays[current_client-1]**2)
+
         clients_value_per_policy.append(client_avg_over_runs)
-
+        
     clients_value_per_policy = np.sum(np.array(clients_value_per_policy), axis=0)
 
 
@@ -175,13 +180,15 @@ for current_client in num_clients:
     for current_policy in policies:
 
         y = average_results(current_policy, current_client) # averaged value for a policy for n number of clients.
-        print(y)
-        y = y / np.arange(1, timeslots+plotting_interval, plotting_interval)
+        #print(y)
+        #y = y / np.arange(1, timeslots+plotting_interval, plotting_interval)
+        y[0] = 0
 
         axs[i].plot(x, y, label=labels[selected_label], color = empirical_colors[selected_label], linestyle=empirical_styles[selected_label])
 
         
         theoretical_value = plot_theoretical_values(current_policy, current_client)
+        print(theoretical_value)
         
         axs[i].axhline(xmin=0, xmax=timeslots, y=theoretical_value, color=theoretical_colors[theoretical_label_count], linestyle=theoretical_styles[theoretical_label_count], label=theoretical_labels[theoretical_label_count])
         theoretical_label_count = theoretical_label_count + 1
@@ -191,15 +198,17 @@ for current_client in num_clients:
 
     # set the title and axis labels
     #axs[i].set_title('N = {}'.format(current_client))
-    if i == 0:
+
         
-        axs[i].legend()
+    axs[i].legend()
 
     axs[i].set_xlabel('Timeslots $t$', size=9)
     axs[i].set_ylabel(r'$\sum_i \overline{outage}_i + \alpha_i \cdot l_i^2$', size=9)
 
     axs[i].set_xticks(np.arange(0,timeslots+graph_interval-1,graph_interval))
-    axs[i].set_ylim([0, 0.0001])
+    axs[0].set_ylim([0, 0.002])
+    axs[1].set_ylim([0, 0.0002])
+    axs[2].set_ylim([0, 0.0005])
     
     #handles, labels = axs[0].get_legend_handles_labels()
     #print(handles)
